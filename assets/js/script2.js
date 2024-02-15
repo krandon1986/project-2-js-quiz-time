96bf2bac4772f4074dda42326cc6f64ea3ed2882
krandon1986 <krandon1986@yahoo.co.uk>
2024-02-15 01:17:19 +0000

    Create a second javascript file and add several questions and answer in it

---
+++ assets/js/script2.js
@@ -0,0 +1,120 @@
+//Questions and Answers Objects
+const questions = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "Paris", correct: true },
+            { text: "Berlin", correct: false },
+            { text: "Lyon", correct: false },
+            { text: "Monte Carlo", correct: false },
+        ]
+    },
+    {
+        question: "In the Metal Gear Solid Series, which of the Snakes is not in the game?",
+        answers: [
+            { text: "Liquid", correct: false },
+            { text: "Solidus", correct: false },
+            { text: "Steam", correct: true },
+            { text: "Solid", correct: false },
+        ]
+    },
+    {
+        question: "What is the starting and ending year of World War II?",
+        answers: [
+            { text: "1920 - 1926", correct: false },
+            { text: "1950 - 1956", correct: false },
+            { text: "1931 - 1937", correct: false },
+            { text: "1939 - 1945", correct: true },
+        ]
+    },
+    {
+        question: "What is the official name for Big Ben?",
+        answers: [
+            { text: "Victoria Tower", correct: false },
+            { text: "Charles Tower", correct: false },
+            { text: "Elizabeth Tower", correct: true },
+            { text: "Philip Tower", correct: false },
+        ]
+    },
+    {
+        question: "What is the largest planet in the Solar System?",
+        answers: [
+            { text: "Neptune", correct: false },
+            { text: "Jupiter", correct: true },
+            { text: "Saturn", correct: false },
+            { text: "Uranus", correct: false },
+        ]
+    },
+    {
+        question: "What year was the Empire State Building built?",
+        answers: [
+            { text: "1931", correct: true },
+            { text: "1924", correct: false },
+            { text: "1914", correct: false },
+            { text: "1940", correct: false },
+        ]
+    },
+    {
+        question: "What is the largest mammal in the world?",
+        answers: [
+            { text: "Elephant", correct: false },
+            { text: "Saltwater Crocodile", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Common Ostrich", correct: false },
+        ]
+    },
+    {
+        question: "Where did the Titanic sink?",
+        answers: [
+            { text: "North Pacific Ocean", correct: false },
+            { text: "North Atlantic Ocean", correct: true },
+            { text: "South Pacific Ocean", correct: false },
+            { text: "South Atlantic Ocean", correct: false },
+        ]
+    },
+    {
+        question: "What year was YouTube created?",
+        answers: [
+            { text: "2000", correct: false },
+            { text: "2010", correct: false },
+            { text: "2015", correct: false },
+            { text: "2005", correct: true },
+        ]
+    },
+    {
+        question: "Where did the Black Death orignated from?",
+        answers: [
+            { text: "England", correct: false },
+            { text: "China", correct: true },
+            { text: "Spain", correct: false },
+            { text: "Italy", correct: false },
+        ]
+    },
+    {
+        question: "Where did the Great Fire of London started from?",
+        answers: [
+            { text: "Puddling Lane", correct: true },
+            { text: "Baker Street", correct: false },
+            { text: "Savile Row", correct: false },
+            { text: "Oxford Street", correct: false },
+        ]
+    },
+    {
+        question: "How many years did the Stone Age last for?",
+        answers: [
+            { text: "1.2 Million", correct: false },
+            { text: "2.3 Million", correct: false },
+            { text: "3.4 Million", correct: true },
+            { text: "4.5 Million", correct: false },
+        ]
+    },
+    {
+        question: "When was the European Union formed?",
+        answers: [
+            { text: "1963", correct: false },
+            { text: "1973", correct: false },
+            { text: "1983", correct: false },
+            { text: "1993", correct: true },
+        ]
+    }
+];
